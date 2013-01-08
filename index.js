749f8ef56788c6619a74da8aae0e397d1bc47c71
jerome creignou <jerome.creignou@gmail.com>
2013-01-08 21:55:02 +0100

    fix compile

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('http').createServer(function(req,res){ 
   res.writeHead(200,{'Content-Type':'text/plain'});
   res.end('jerome.creignou@gmail.com','utf-8');
-}).listen(process.env.PORT||5000, function(){
-  console.log("Listening on " + port);
+}).listen(process.env.PORT||5000, function(port){
+  console.log("Listening ...");
 });
