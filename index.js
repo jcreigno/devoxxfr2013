202ac36163efaab140647d734fc2b94534d00b57
jerome creignou <jerome.creignou@gmail.com>
2013-01-08 21:42:20 +0100

    add logs

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 require('http').createServer(function(req,res){ 
   res.writeHead(200,{'Content-Type':'text/plain'});
   res.end('jerome.creignou@gmail.com','utf-8');
-}).listen(process.env.PORT||8080);
+}).listen(process.env.PORT||5000, function(){
+  console.log("Listening on " + port);
+});
