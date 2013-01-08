339e2a067653ffb764105b58b2036ec83da75947
jerome creignou <jerome.creignou@gmail.com>
2013-01-08 10:42:33 +0100

    initial commit for code story

---
+++ index.js
@@ -0,0 +1,4 @@
+require('http').createServer(function(req,res){ 
+  res.writeHead(200,{'Content-Type':'text/plain'});
+  res.end('jerome.creignou@gmail.com','utf-8');
+}).listen(process.env.PORT||8080);
