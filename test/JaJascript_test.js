4f1c5446618c70a4a80bd180f7a80522548d0906
jerome creignou <jerome.creignou@free.fr>
2013-01-15 08:17:54 -0500

    ajout du tri de l'entrée.

--- test/JaJascript_test.js
+++ test/JaJascript_test.js
@@ -40,8 +40,37 @@ vows.describe('L\'entreprise location JaJascript de Martin O.').addBatch({
     }]).optimize(),
     'on répond { gain: 18, path: [ \'MONAD42\', \'LEGACY01\' ] }': assertResult(18, ["MONAD42", "LEGACY01"])
   },
+  ' avec l\'exemple de l\'énoncé dans le désordre ': {
+    topic: JaJascript([{
+      "VOL": "LEGACY01",
+      "DEPART": 5,
+      "DUREE": 9,
+      "PRIX": 8
+    }, {
+      "VOL": "MONAD42",
+      "DEPART": 0,
+      "DUREE": 5,
+      "PRIX": 10
+    }, {
+      "VOL": "YAGNI17",
+      "DEPART": 5,
+      "DUREE": 9,
+      "PRIX": 7
+    }, {
+      "VOL": "META18",
+      "DEPART": 3,
+      "DUREE": 7,
+      "PRIX": 14
+    }]).optimize(),
+    'on obtient le même résultat': assertResult(18, ["MONAD42", "LEGACY01"])
+  },
   'avec le premier exemple {"VOL": "AF514", "DEPART":0, "DUREE":5, "PRIX": 10}': {
-    topic: JaJascript([ {"VOL": "AF514", "DEPART":0, "DUREE":5, "PRIX": 10}]).optimize(),
+    topic: JaJascript([{
+      "VOL": "AF514",
+      "DEPART": 0,
+      "DUREE": 5,
+      "PRIX": 10
+    }]).optimize(),
     'on répond AF514 ': assertResult(10, ["AF514"])
   }
 }).export(module);
