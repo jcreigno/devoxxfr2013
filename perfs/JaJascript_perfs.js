09885f919a7c7fb426bdd5c866f3a819899d7317
jerome creignou <jerome.creignou@gmail.com>
2013-01-17 14:57:13 +0100

    remove array copy

--- perfs/JaJascript_perfs.js
+++ perfs/JaJascript_perfs.js
@@ -26,6 +26,7 @@ function assertGain(gain) {
 }
 
 var input = require('./10AF123.json');
+var sample = require('./sample.json');
 
 vows.describe('L\'entreprise location JaJascript de Martin O.').addBatch({
   ' test de performances avec une entrée importante ': {
@@ -35,5 +36,23 @@ vows.describe('L\'entreprise location JaJascript de Martin O.').addBatch({
     })(),
     'on répond {"gain":48,"path":["AF11","AF14","AF33","AF36","AF39","AF312","AF315","AF318","AF214","AF217"]}': 
       assertGain(48)
+  },
+  ' test de performances avec une vrai requête ': {
+    topic: (function(){
+      console.time("vows-optim");
+      return JaJascript(sample).optimize()
+    })(),
+    'on trouve un gain 167':
+      assertResult(167,
+        ["important-warehouse-88", 
+         "old-luggage-67", 
+         "faint-sculpture-1", 
+         "clever-frisbee-57", 
+         "jolly-archery-8", 
+         "lonely-ship-5", 
+         "difficult-menthol-61", 
+         "tired-rating-22", 
+         "chubby-protein-48"]
+      )
   }
 }).export(module);
