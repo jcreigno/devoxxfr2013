e6848dfada94f76e081f7547116292947cbaa3ab
jerome creignou <jerome.creignou@gmail.com>
2015-06-15 13:51:14 +0200

    Update Simple_test.js

--- test/Simple_test.js
+++ test/Simple_test.js
@@ -76,7 +76,7 @@ var questions = {
   '(((1,1+2)+3,14+4+(5+6+7)+(8+9+10)*4267387833344334647677634)/2*553344300034334349999000)/31878018903828899277492024491376690701584023926880': '1',
   'As tu passe une bonne nuit malgre les bugs de l etape precedente(PAS_TOP/BOF/QUELS_BUGS)': 'PAS_TOP',
   'As tu bien recu le second enonce(OUI/NON)': 'OUI',
-  'Veux+tu+encore+jouer(OUI|NON)':'OUI'
+  'Veux+tu+encore+jouer(OUI|NON)':'NON'
 };
 
 
